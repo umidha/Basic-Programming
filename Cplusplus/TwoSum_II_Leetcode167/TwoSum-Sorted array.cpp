506ec6c493b51735ed0a36fb798e6b98b0859522
umidha <uday.midha@gmail.com>
2016-09-23 18:49:57 -0600

    Update TwoSum-Sorted array.cpp

--- Cplusplus/TwoSum_II_Leetcode167/TwoSum-Sorted array.cpp
+++ Cplusplus/TwoSum_II_Leetcode167/TwoSum-Sorted array.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
+#include <vector>
 
-int main(){
-        std::vector<int> input = {1,2,4,7,10};
-        target = 11;
-        int a = 0;
-        int b = numbers.size() - 1;
-        int sum;
-        sum = numbers[a] + numbers[b];
-        while(a < b && sum != target){
-            if(sum < target)
-                a++;
-            else
-                b--;
-            sum = numbers[a] + numbers[b];
-        }
-        return vector<int>{a+1, b+1};
+
+int main()
+{
+	std::vector<int> input = { 1,2,4,6,10 };
+	int target = 11;
+	int a = 0;
+        int b = input.size() - 1;
+	int sum;
+	sum = input[a] + input[b];
+	while (a < b && sum != target) {
+		if (sum < target)
+			a++;
+		else
+			b--;
+		sum = input[a] + input[b];
+	}
+	std::cout << "The indices are " << a + 1 << " " << b + 1 << std::endl;
+
+	return 0;
 }
